6f61597ce09f02ec93cbcd7c67ff442abcfd79c3
Truongminhdi96 <truongvy689@gmail.com>
2025-11-16 00:37:08 +0700

    Tao trang cap nhat lien he

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -2,10 +2,18 @@ import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
+  // Trang danh bạ
   {
     path: "/",
     name: "contactbook",
     component: ContactBook,
+  },
+  // Trang cập nhật liên hệ
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // truyền id từ $route.params vào props
   },
   // Route 404 - luôn để cuối cùng
   {
