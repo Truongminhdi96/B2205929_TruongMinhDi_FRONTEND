47e4573dc4fcbbd3544b4854d80eed15d6913dd5
Truongminhdi96 <truongvy689@gmail.com>
2025-11-16 00:03:40 +0700

    Hien thi danh sach cac contact lay ve tu server

--- contactbook-frontend/vite.config.js
+++ contactbook-frontend/vite.config.js
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Thêm dòng này
+    },
+  },
   server: {
-    port: 3001
-  }
-})
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
